0c7375ec92d60a5dd72e5014b8b28c77218f8cd4
Awaliyatul Hikmah <imadlz15@gmail.com>
2025-01-17 17:04:20 +0700

    add mock data

---
+++ src/api/auth/authRepository.ts
@@ -0,0 +1,24 @@
+import type { Auth } from "@/api/auth/authModel";
+
+export const registeredUser: Auth[] = [
+  {
+    id: "e5a03e0c-b6e2-4318-b688-a0c4373a5ae4",
+    name: "Awaliyatul Hikmah",
+    email: "imadlz15@gmail.com",
+    password: null,
+    avatar: "https://lh3.googleusercontent.com/a/ACg8ocIwcFrWoislM17T9Yp9Kuh1_Zp5eEslvqNykzXBABuTbaJvc_d6vg=s96-c",
+    planId: null,
+    createdAt: new Date("2025-01-15T05:04:36.259Z"),
+    updatedAt: null,
+  },
+  {
+    id: "ab188782-3f97-40c3-bd68-30f7959196ba",
+    name: "Hikmah",
+    email: "hikmah@mail.com",
+    password: "$2b$10$VcRbOreifdE7WxPJaHxclu.Ff9nTjGUK6f2qM3C58vKYeo9Q9uaXC",
+    avatar: null,
+    planId: "17bec569-60a7-4291-a4ea-82309d96da3f",
+    createdAt: new Date("2025-01-15T06:20:56.834Z"),
+    updatedAt: null,
+  },
+];
---
+++ src/api/planner/plannerRepository.ts
@@ -0,0 +1,24 @@
+import type { Planner } from "@/api/planner/plannerModel";
+
+export const planners: Planner[] = [
+  {
+    id: "ef4ba7d9-4824-44b3-9513-95d262bc4b95",
+    pagePerDay: 0.5,
+    isActive: false,
+  },
+  {
+    id: "4d5bab06-048e-4247-a5aa-3c1cb7c42be2",
+    pagePerDay: 2.0,
+    isActive: false,
+  },
+  {
+    id: "93dfc067-d061-457b-8b25-1fa61bd5db92",
+    pagePerDay: 3.0,
+    isActive: false,
+  },
+  {
+    id: "17bec569-60a7-4291-a4ea-82309d96da3f",
+    pagePerDay: 1.0,
+    isActive: true,
+  },
+];
