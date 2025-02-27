9e81e81d5d2c1e6288cc0cfea7ebc00811028bc1
Awaliyatul Hikmah <imadlz15@gmail.com>
2025-02-27 17:54:04 +0700

    update API title in OpenAPI document generator to "Tikrar Journal API"

--- src/api-docs/openAPIDocumentGenerator.ts
+++ src/api-docs/openAPIDocumentGenerator.ts
@@ -11,7 +11,7 @@ export function generateOpenAPIDocument() {
     openapi: "3.0.0",
     info: {
       version: "1.0.0",
-      title: "Swagger API",
+      title: "Tikrar Journal API",
     },
     externalDocs: {
       description: "View the raw OpenAPI Specification in JSON format",
